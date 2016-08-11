3c2413d34e388d53dcc48d269e24e8e6952a19d3
Mark Pajak <markpajak.spider@gmail.com>
2016-08-11 11:08:57 +0100

    Update startChrome.bat

--- startChrome.bat
+++ startChrome.bat
@@ -1,6 +1,6 @@
 @echo off
 taskkill /F /IM chrome.exe
 
-start "Chrome" chrome "file:%~dp0index.html" --allow-file-access-from-files --incognito --kiosk --disable-direct-write
+start "Chrome" chrome "file:%~dp0index.html/PPL-CR-ICT05" --allow-file-access-from-files --incognito --kiosk --disable-direct-write
 
 
