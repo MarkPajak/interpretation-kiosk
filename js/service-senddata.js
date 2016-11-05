b52a496f2a93caeefd7f1ef8500d688187452781
Mark Pajak <markpajak.spider@gmail.com>
2016-11-05 15:37:20 +0000

    Update service-senddata.js

--- js/service-senddata.js
+++ js/service-senddata.js
@@ -13,6 +13,7 @@ museum_objectcatServices.factory('send_data', ['$http', function ($http) {
     StudentDataOp.add_log = function (page) {
 	    
 	    page.app_type="INTERPRETATION-KIOSK";
+	     page.location="MSHED";
 		
 		  return $.ajax({
             type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
