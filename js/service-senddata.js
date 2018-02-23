db6c9005036fde1060eb3195f9ee1c8a8c90cdae
Mark Pajak <markpajak.spider@gmail.com>
2018-02-23 10:12:24 +0000

    Update service-senddata.js

--- js/service-senddata.js
+++ js/service-senddata.js
@@ -3,7 +3,7 @@
 
 museum_objectcatServices.factory('send_data', ['$http', function ($http) {
 
-    var urlBase =  'http://markpajak.co.uk/mark/kiosk-feedback/user_analytics.php';
+   
     var StudentDataOp = {};
 
     StudentDataOp.getStudents = function () {
@@ -15,11 +15,14 @@ museum_objectcatServices.factory('send_data', ['$http', function ($http) {
 	    page.app_type="INTERPRETATION-KIOSK";
 	     page.location="MSHED";
 		
-		  return $.ajax({
-            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-            url         : urlBase, // the url where we want to POST
+			
+
+		return $.ajax({
+			type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : 'http://performance.bristolmuseums.org.uk/feedback/new', // the url where we want to POST
             data        : page, // our data object
-            dataType    : 'json', // what type of data do we expect back from the server
+            dataType    : 'jsonp', // what type of data do we expect back from the server	
+
              encode          : true
         })
 		
